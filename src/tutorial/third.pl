ee5b5d83d5b37c48fa37f348e92f05c5af213564
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2004-06-12 14:50:53 +0000

    Added screenshots to tutorial.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/WebSite/trunk@1282 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

---
+++ src/tutorial/third.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Wx;
+
+#<!-- replace #1 -->
+package MyFrame;
+
+use base 'Wx::Frame';
+# import the event registration function
+use Wx::Event qw(EVT_BUTTON);
+#<!-- end -->
+
+sub new {
+    my $ref = shift;
+    my $self = $ref->SUPER::new( undef,           # parent window
+                                 -1,              # ID -1 means any
+                                 'wxPerl rules',  # title
+                                 [-1, -1],        # default position
+                                 [150, 100],      # size
+                                 );
+
+    my $panel = Wx::Panel->new( $self,            # parent window
+                                -1,               # ID
+                                );
+#<!-- replace #2 -->
+    my $button = Wx::Button->new( $panel,         # parent window
+                                  -1,             # ID
+                                  'Click me!',    # label
+                                  [30, 20],       # position
+                                  [-1, -1],       # default size
+                                  );
+
+    # register the OnClick method as an handler for the
+    # 'button clicked' event. The first argument is a Wx::EvtHandler
+    # that receives the event
+    EVT_BUTTON( $self, $button, \&OnClick );
+
+    return $self;
+}
+#<!-- end -->
+
+# this method receives as its first parameter the first argument
+# passed to the EVT_BUTTON function. The second parameter
+# always is a Wx::Event subclass
+#<!-- replace #3 -->
+sub OnClick {
+    my( $self, $event ) = @_;
+
+    $self->SetTitle( 'Clicked' );
+}
+#<!-- end -->
+
+package MyApp;
+
+use base 'Wx::App';
+
+sub OnInit {
+    my $frame = MyFrame->new;
+
+    $frame->Show( 1 );
+}
+
+package main;
+
+my $app = MyApp->new;
+$app->MainLoop;
