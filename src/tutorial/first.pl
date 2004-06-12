285e4aa2ab329c713b55062b319fb52769da92fd
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2004-06-12 12:07:06 +0000

    Rewrittend remaining chapters of the tutorial with their examples.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/WebSite/trunk@1278 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- src/tutorial/first.pl
+++ src/tutorial/first.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/perl -w
+
 use strict;
 
 #<!-- replace #1 -->
@@ -23,6 +25,8 @@ sub OnInit {
     my $frame = Wx::Frame->new( undef,           # parent window
                                 -1,              # ID -1 means any
                                 'wxPerl rules',  # title
+                                [-1, -1],         # default position
+                                [250, 150],       # size
                                );
 #<!-- end -->
 
--- src/tutorial/second.pl
+++ src/tutorial/second.pl
@@ -1,116 +1,56 @@
+#!/usr/bin/perl -w
+
+use strict;
 use Wx;
 
-# this is our own Frame class    
+#<!-- replace #1 -->
 package MyFrame;
     
-use strict;
 use base 'Wx::Frame';
+#<!-- end -->
 
-use Wx::Event qw(EVT_MENU EVT_CLOSE);
-
-my( $ID_ABOUT, $ID_EXIT ) = ( 1, 2 );
-
-# Parameters: title, position, size
+#<!-- replace #2 -->
 sub new {
-  my( $class ) = shift;
-  # now we are calling the base class constructor
-  my( $this ) = $class->SUPER::new( undef, -1, $_[0], $_[1], $_[2] );
+    my $ref = shift;
+    my $self = $ref->SUPER::new( undef,           # parent window
+                                 -1,              # ID -1 means any
+                                 'wxPerl rules',  # title
+                                 [-1, -1],        # default position
+                                 [150, 100],      # size
+                                 );
+#<!-- end -->
 
-  # now that we have an object we can store some data inside of it
-  $this->{mydata} = 'This is some data';
-  $this->{myotherdata} = 123;
+#<!-- replace #3 -->
+    # controls should not be placed directly inside
+    # a frame, use a Wx::Panel instead
+    my $panel = Wx::Panel->new( $self,            # parent window
+                                -1,               # ID
+                                );
+    # create a button
+    my $button = Wx::Button->new( $panel,         # parent window
+                                  -1,             # ID
+                                  'Click me!',    # label
+                                  [30, 20],       # position
+                                  [-1, -1],       # default size
+                                  );
+#<!-- end -->
 
-  # create 2 menus
-  my( $mfile ) = Wx::Menu->new();
-  my( $mhelp ) = Wx::Menu->new();
-
-  # add entries to menus
-  $mhelp->Append( $ID_ABOUT, "&About...\tCtrl-A", "Show about dialog" );
-  $mfile->Append( $ID_EXIT, "E&xit\tAlt-X", "Quit this program" );
-    
-  # create a menu bar
-  my( $mbar ) = Wx::MenuBar->new();
-    
-  # append the menus to the menu bar
-  $mbar->Append( $mfile, "&File" );
-  $mbar->Append( $mhelp, "&Help" );
-    
-  # and assign the menubar to the frame
-  $this->SetMenuBar( $mbar );
-
-  # create a status bar with two fields
-  $this->CreateStatusBar( 2 );
-  # and show a friendly message
-  $this->SetStatusText( "Welcome to wxPerl!", 1 );
-
-  # declare that events coming from menu items with the given
-  # id will be handled by the frame ($this) using the listed methods
-
-  # in wxWindows you will have used EVT_MENU( id, method )
-  # with _two_ parameters, and EVT_CLOSE( method ), with _one_ parameter
-  EVT_MENU( $this, $ID_EXIT, \&OnQuit );
-  EVT_MENU( $this, $ID_ABOUT, \&OnAbout );
-  EVT_CLOSE( $this, \&OnClose );
-
-  return $this;
+    return $self;
 }
 
-# these are some constants used by Wx::MessageBox
-use Wx qw(wxOK wxICON_INFORMATION wxVERSION_STRING);
-
-# called when the user selects the 'Exit' menu item
-sub OnQuit {
-  my( $this, $event ) = @_;
-
-  Wx::MessageBox( 'OnQuit: Exiting', 'Message', wxOK, $this );
-
-  # closes the frame
-  $this->Close( 1 );
-}
-
-# called when the user selects the 'About' menu item
-sub OnAbout {
-  my( $this, $event ) = @_;
-
-  # display a simple about box
-  Wx::MessageBox( "This is the about dialog of minimal sample.\n" .
-                  "Welcome to wxPerl " . $Wx::VERSION . "\n" .
-                  wxVERSION_STRING,
-                  "About minimal", wxOK | wxICON_INFORMATION,
-                  $this );
-}
-
-sub OnClose {
-  my( $this, $event ) = @_;
-
-  Wx::MessageBox( 'OnClose: Closing', 'OnClose', wxOK, $this );
-
-  $event->Skip();
-}
-
+#<!-- replace #4 -->
 package MyApp;
     
-use strict;
 use base 'Wx::App';
     
-use Wx qw(wxDefaultPosition);
-
 sub OnInit {
-  my( $this ) = shift;
-    
-  my( $frame ) = MyFrame->new( "Minimal wxPerl app",
-                               wxDefaultPosition,
-                               [450, 350]
-                             );
-    
-  $this->SetTopWindow( $frame );
+    my $frame = MyFrame->new;
 
   $frame->Show( 1 );
 }
     
 package main;
     
-# create an instance of the Wx::App-derived class
-my( $app ) = MyApp->new();
-# start processing events
-$app->MainLoop();
+my $app = MyApp->new;
+$app->MainLoop;
+#<!-- end -->
